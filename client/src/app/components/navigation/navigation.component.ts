04e5854c207d87553001b619363384b4da5aa989
istvan.iodi <istvan.iodi@accesa.eu>
2024-05-04 18:03:40 +0300

    Dark mode added, icons added to sidebar and navigation

--- client/src/app/components/navigation/navigation.component.ts
+++ client/src/app/components/navigation/navigation.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   
   public isCollapsed = true;
+  public navbarHeight = 0;
+  
+  @ViewChild('navbar', { static: true }) navbar: ElementRef;
+  
+  ngOnInit() {
+    this.navbarHeight = this.navbar.nativeElement.offsetHeight;
+  }
 }
